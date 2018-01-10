b2f2e48a1b954a54f4c6d2e68393959bf9eaf67f
Bianca M Fernandez <bianca.maria.fernandez.22@gmail.com>
2018-01-10 13:32:38 -0800

    Create rails migration of entry belongs to user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,20 @@
-entry_data = JSON.parse(File.read('db/seeds/entries.json'));
-
-
-entry_data.each do |entry|
-  Entry.create!(entry)
-  puts "Entry created"
-end
-
-user_data = JSON.parse(File.read('db/seeds/users.json'))
-
-user_data.each do |user|
-  User.create!(user)
-  puts "User created"
-end
+# entry_data = JSON.parse(File.read('db/seeds/entries.json'));
+#
+#
+# # entry_data.each do |entry|
+# #   new_entry = Entry.new(entry)
+# #   puts "Entry created"
+# # end
+#
+# user_data = JSON.parse(File.read('db/seeds/users.json'))
+#
+# user_data.each do |user|
+#   new_user = User.new(user)
+#   puts "User created"
+#   entry_data.each do |entry|
+#     new_entry = Entry.new(entry)
+#     new_entry.user = new_user
+#     new_entry.save
+#   end
+#   new_user.save
+# end
