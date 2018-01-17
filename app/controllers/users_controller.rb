8afa4b8a9a90debeb4f2b1bed2534270d7c14659
Bianca M Fernandez <bianca.maria.fernandez.22@gmail.com>
2018-01-17 10:48:05 -0800

    Add login and sign up paths to user controller and routes with some adjustments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,53 +1,68 @@
 class UsersController < ApplicationController
   protect_from_forgery with: :null_session
 
-  def index
-    users = User.all
-    render json: users, status: :ok
-  end
+  def login
+    user = User.find_by_email(params[:email])
 
-  def show
-    # render(
-    # status: :ok,
-    # json:
-    # )
-  end
-
-  def create
-    #need to create conditional -- search for user, if exists, return user id
-    # if doesn't exit, create new user and give visual feedback
-    user = User.find_by(params[:email])
-
-    #    @merchant = Merchant.find_by(id: expected_merchant_id)
-
-
-    if user.save
-      render json: { name: user.name, email: user.email, password: user.password, user_id: user.id }, status: :ok
+    if user
+      render json: { email: user.email, user_id: user.id },
+      status: :ok
     else
-      render json: { errors: user.errors.messages },
-        status: :bad_request
+      render json: {errors: "Username does not exist in our records"},
+      status: :bad_request
     end
-
-
-
-    # @user = User.new(user_params)
-    #       if @user.save
-    #         render text: @user.access_token, status: 201
-    #       else
-    #         render json: @user.errors, status: 422
-    #       end
   end
 
-  def update
-  end
+  def signup
+    user = User.new(user_params)
 
-  def destroy
+    if User.find_by_email(params[:email])
+      render json: {errors: "email already exists in our records"},
+      status: :bad_request
+    else
+      if user.save
+        render json: { name: user.name, email: user.email, password: user.password, user_id: user.id }, status: :ok
+      else
+        render json: { errors: user.errors.messages },
+        status: :bad_request
+      end
+    end
   end
-
 
   private
-    def user_params
-      params.require(:user).permit(:name, :email, :password)
-    end
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
 
 end
+
+  # def create
+  #need to create conditional -- search for user, if exists, return user id
+  # if doesn't exit, create new user and give visual feedback
+  # user = User.find_by(params[:email])
+
+  #    @merchant = Merchant.find_by(id: expected_merchant_id)
+
+
+  # if user.save
+  #   render json: { name: user.name, email: user.email, password: user.password, user_id: user.id }, status: :ok
+  # else
+  #   render json: { errors: user.errors.messages },
+  #     status: :bad_request
+  # end
+
+
+
+  # @user = User.new(user_params)
+  #       if @user.save
+  #         render text: @user.access_token, status: 201
+  #       else
+  #         render json: @user.errors, status: 422
+  #       end
+  # end
+
+  #
+  # def index
+  #   users = User.all
+  #   render json: users, status: :ok
+  # end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :entries
   #
-  resources :users
+  # resources :users
   #   get '/entries', to: 'entries#index'
   # end
 
